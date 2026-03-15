756fe247990e3a9214f2efe405c9a521572e9d95
Evan Chan <evanchan0819@gmail.com>
2026-03-15 20:46:07 +0800

    Revert "Bump VDSO_PAGES from 2 to 3 for kernel 6.x compatibility"
    This reverts commit 622cd2ed66f527836897b41ca013fb9bc260e5b5.

--- tools/ptraceomatic-config.h
+++ tools/ptraceomatic-config.h
@@ -1,4 +1,2 @@
 #define VVAR_PAGES 4
-// Kernel 6.x+ uses 3 VDSO pages for 32-bit; older kernels use 2.
-// Using 3 here is safe on older kernels (extra page is just zeroed).
-#define VDSO_PAGES 3
+#define VDSO_PAGES 2
